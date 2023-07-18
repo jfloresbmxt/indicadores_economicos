8a91fe696317d082a624a17f06dc9e10b7707f4b
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-07-18 11:28:22 -0600

    Agrega requirements

--- Introduccion.py
+++ Introduccion.py
@@ -1,8 +1,4 @@
 import streamlit as st
-from pandas import read_excel
-from css.metrics import metrics
-from tables.pib import gen_tabla1, gen_tabla2, gen_tabla3, gen_tabla5
-from tables.anuies import get_database, get_tables
 
 st.set_page_config(
     page_title="Información estatal",
